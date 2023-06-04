7a1eb78cf45fae8f33430abe616b0c5edb4cb00f
pomelott <pomelott@163.com>
2023-06-04 23:06:29 +0800

    feat: daily learn

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(str, Command)>) -> Vec<str> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<str> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    string = string.to_uppercase();
+                },
+                Command::Trim => {
+                    string = string.trim();
+                },
+                Command::Append(num) => {
+                    let mut count = num;
+                    while count > &0 {
+                        string += "baz";
+                        count = &(count - 1);
+                    }
+                }
+            }
+            output.push(string);
         }
         output
     }
@@ -43,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::*;
     use super::Command;
 
     #[test]
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,9 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let number = String::from("T-H-R-E-E"); // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    let add_number = "F-O-U-R"; // don't rename this variable
+    println!("Number plus two is : {}", number + add_number);
 }
